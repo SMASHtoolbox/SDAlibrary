b79f3e028784fcf901e674117a6702c4cab4227d
John Kaushagen <jkausha@sandia.gov>
2017-04-06 13:52:24 -0600

    add a basic wrapper structure for the h5py.File type

--- python/pysda/SDAFile.py
+++ python/pysda/SDAFile.py
@@ -1,3 +1,15 @@
-class SDAFile:
-    def __init__(self):
-        pass
+import h5py
+
+
+class SDAFile(h5py.File):
+    """The main entry point for interacting with the SDA format.
+    This file object contains most variables and methods needed
+    for using the file. Note that the data is provided in a 'raw'
+    format, so the user is responsible for interprating the data
+    in a manner useful to them. Specifically note that some of the
+    pre-defined data-type the SDA library uses are matlab functions
+    and may not be compatible with python. This issue may be
+    addressed at a later time.
+    """
+    def __init__(self, file_name, mode='r'):
+        super(SDAFile, self).__init__(file_name, mode=mode)
