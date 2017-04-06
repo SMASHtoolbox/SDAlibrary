cfe011e46c9a3e4d640412b36678c8fccecb4bf4
John Kaushagen <jkausha@sandia.gov>
2017-04-06 14:36:47 -0600

    change project structure

--- python/pysda.py
+++ python/pysda.py
@@ -23,72 +23,91 @@ class SDAFile(h5py.File):  # pylint: disable=too-many-ancestors
     """
     def __init__(self, file_name, mode='r'):
         super(SDAFile, self).__init__(file_name, mode=mode)
-    
+        self.file_format = self.attrs['FileFormat']
+        self.format_verion = self.attrs['FormatVersion']
+        self.created = self.attrs['Created']
+        self.updated = self.attrs['Updated']
+        self.writable = self.attrs['Writable']
+
     # TODO: Implement Type checking for each record
-
-
-
-
-
-
-
-
-
 
 # Record Defintions
 
-class Numeric:
+
+class Record:
     def __init__(self):
         pass
 
 
-class Logical:
+class Numeric(Record):
+    """Numeric arrays of arbitrary size/dimension."""
+    def __init__(self):
+        self.record_type = None
+        self.empty = None
+        self.description = None
+        self.deflate = None
+        self.complex = None
+        self.sparse = None
+        self.command = None
+        pass
+
+
+class Logical(Record):
+    """Logical arays of arbitrary size/dimension."""
     def __init__(self):
         pass
 
 
-class Character:
+class Character(Record):
+    """Character arrays of arbitrary size/dimension."""
     def __init__(self):
         pass
 
 
-class Function:
-    def __init__(self):
-        pass
-        
-
-class Cell:
+# This may need to be removed.
+class Function(Record):
+    """MATLAB function handles."""
     def __init__(self):
         pass
 
 
-class Structure:
-    def __init__(self):
-        pass
-        
-
-class Structures:
-    def __init__(self):
-        pass
-        
-        
-class Object:
+class Cell(Record):
+    """Cell arrays of arbitrary size/dimension."""
     def __init__(self):
         pass
 
 
-class Objects:
+class Structure(Record):
+    """Structured data."""
     def __init__(self):
         pass
 
 
-class File:
+class Structures(Record):
+    """Structured data array."""
     def __init__(self):
         pass
 
 
-class Split:
+class Object(Record):
+    """Custom MATLAB object."""
     def __init__(self):
         pass
 
 
+class Objects(Record):
+    """Custom MATLAB object array."""
+    def __init__(self):
+        pass
+
+
+class File(Record):
+    """File stored inside an archive."""
+    def __init__(self):
+        pass
+
+
+class Split(Record):
+    """File split across multiple archives."""
+    def __init__(self):
+        pass
