b21c809480a1230ef31b7fb124be0eca8f3975bf
John Kaushagen <jkausha@sandia.gov>
2017-04-07 09:11:07 -0600

    update decorator

--- python/tests/test_pysda.py
+++ python/tests/test_pysda.py
@@ -1,23 +1,31 @@
+import time
+
 from nose.tools import *
 import pysda
 
 
-def stop_time(f):
-    original = time.localtime
+def freeze_decorator(time_to_freeze):
+    def function_decorator(func):
+        original = time.localtime
 
-    def newf(*args, **kwargs):
-        now = original()
-        time.localtime = lambda: now
-        result = f(*args, **kwargs)
-        time.localtime = original
-        return result
-    return newf
+        def newf(*args, **kwargs):
+            time.localtime = lambda: time_to_freeze
+            result = f(*args, **kwargs)
+            time.localtime = original
+            return result
+        return newf
+    return function_decorator
 
 
 class TestPySDA(object):
+    test_time = 1483254000  # January 1st, 2017 00:00:00
+
     def __init__(self):
         pass
 
-    def test_file_format_check(self):
-        with assert_raises(IOError):
-            f = pysda.SDAFile('bad_file.sda', mode='r')
+    @freeze_decorator(test_time)
+    def test_created_time(self):
+        f = pysda.SDAFile('test.sda', 'w')
+        expected = time.localtime(TestPySDA.test_time)
+        actual = f.attrs['Created']
+        assert_equal(actual, expected)
